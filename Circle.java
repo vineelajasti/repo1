e160312231a829e79c6f9f132db6044847af9dc9
Jasti <vineela.jasti@dxc.com>
2020-08-11 20:58:12 -0700

    main is added

---
+++ Circle.java
@@ -0,0 +1,12 @@
+public class Circle 
+{
+     private double radius;
+     public Circle(double radius)
+     {
+    	 this.radius=radius;
+     }
+     public double area()
+     {
+    	 return 3.14*radius*radius;
+     }
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main 
+{
+     public static void main(String[] args){
+         Circle c = new Circle(5.4);
+         System.out.println(c.area());
+     }
+}
